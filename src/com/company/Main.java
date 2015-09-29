a232fda886a2f850171233bedc9b43d2bed2edfe
crz <tryserg@gmail.com>
2015-09-29 13:35:42 +0300

    update arrays

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,33 +2,50 @@ package com.company;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] arr1 = new int[5];
-        int[] arr2 = new int[5];
+        int[][] arr1 = new int[5][5];
+        Random rn = new Random();
 
-        try (PrintWriter pw = new PrintWriter("a.txt")) {
-            for (int i = 0; i < arr1.length; i++) {
-                System.out.println("arr1 element #" + i);
-                arr1[i] = sc.nextInt();
-
-                System.out.println("arr2 element #" + i);
-                arr2[i] = sc.nextInt();
-                
-                pw.println(arr1.toString());
-                pw.println(arr2.toString());
-
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[i].length; j++) {
+                arr1[i][j] = rn.nextInt(9);
             }
 
+        }
+        printInConcole(arr1);
+        saveToFile(arr1);
+    }
+
+
+    static void saveToFile(int[][] array) {
+        try (PrintWriter pw = new PrintWriter("array.txt")) {
+            for (int[] i : array) {
+                for (int j : i) {
+                    pw.print(j + " ");
+                }
+                pw.println();
+            }
+            System.out.println("Array saved to array.txt");
+
         } catch (IOException e) {
-            System.out.println("ERROR");
+            System.out.println("error");
         }
 
-
-        // write your code here
     }
+
+
+    static void printInConcole(int[][] array) {
+        for (int[] i : array) {
+            for (int j : i) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
 }
